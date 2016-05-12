70121947da86e91f2bb630642346ba00871afdf3
Paul <pcruz96@yahoo.com>
2016-05-12 11:29:17 -0700

    upd test suite

--- src/test/java/com/automation/tests/project/Suite.java
+++ src/test/java/com/automation/tests/project/Suite.java
@@ -1,5 +1,6 @@
 package com.automation.tests.project;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.automation.selenium.Driver;
@@ -7,15 +8,16 @@ import com.automation.tests.BaseTest;
 
 public class Suite extends BaseTest {
 
-	@Test(groups={"smoke"}, enabled=true)
+	@Test(groups={"Acceptance"}, enabled=true)
 	public void c1_tc1() {
 		System.out.println("step 1a");
 		Driver.getDriver().get("http://www.yahoo.com");
 	}
 
-	@Test(groups={"smoke"}, enabled=true)
+	@Test(groups={"Other"}, enabled=true)
 	public void c2_tc2() {
 		System.out.println("step 1b");
 		Driver.getDriver().get("http://www.amazon.com");
+		Assert.assertTrue(false);
 	}
 }
