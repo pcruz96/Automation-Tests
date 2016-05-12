bb2d55aebcba18013e325026b664a7c51e53be79
Paul <pcruz96@yahoo.com>
2016-05-12 11:15:00 -0700

    test sauce labs

--- src/test/java/com/automation/tests/project/Suite.java
+++ src/test/java/com/automation/tests/project/Suite.java
@@ -2,6 +2,7 @@ package com.automation.tests.project;
 
 import org.testng.annotations.Test;
 
+import com.automation.selenium.Driver;
 import com.automation.tests.BaseTest;
 
 public class Suite extends BaseTest {
@@ -9,10 +10,12 @@ public class Suite extends BaseTest {
 	@Test(groups={"smoke"}, enabled=true)
 	public void c1_tc1() {
 		System.out.println("step 1a");
+		Driver.getDriver().get("http://www.yahoo.com");
 	}
 
 	@Test(groups={"smoke"}, enabled=false)
 	public void c2_tc2() {
 		System.out.println("step 1b");
+		Driver.getDriver().get("http://www.amazon.com");
 	}
 }
