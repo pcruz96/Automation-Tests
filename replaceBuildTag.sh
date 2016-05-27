d27adab519065b21dbd5572101eee9aea1e2fe2a
Paul Cruz <pcruz96@yahoo.com>
2016-05-27 06:07:15 -0700

    revert last changes

--- replaceBuildTag.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-echo $SUITE
-sed -i.tmp "s/BUILD_TAG/$SUITE/g" $BUILD_NUMBER/artifact/src/test/resources/testng/testng.xml
-sed -i.tmp "s/BUILD_TAG/$SUITE/g" $BUILD_NUMBER/artifact/target/surefire-reports/emailable-report.html
