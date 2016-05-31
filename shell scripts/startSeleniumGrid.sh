29f55f8750e51c9de38be54c7d5b8c48577958ba
Paul Cruz <pcruz96@yahoo.com>
2016-05-31 08:20:33 -0700

    update startSeleniumGrid.sh

--- shell scripts/startSeleniumGrid.sh
+++ shell scripts/startSeleniumGrid.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
-java -jar src/test/resources/selenium/selenium-server-standalone-2.53.0.jar -role hub | \
-java -jar -Dwebdriver.chrome.driver="src/test/resources/selenium//chromedriver" src/test/resources/selenium/selenium-server-standalone-2.53.0.jar -role node -hub http://localhost:4444/grid/register \
+jarPath="../src/test/resources/selenium/selenium-server-standalone-2.53.0.jar"
 
-#-maxSession 25 -browser browserName=chrome,maxInstances=25
+java -jar $jarPath -role hub | \
+java -jar -Dwebdriver.chrome.driver="../src/test/resources/selenium/chromedriver.exe" $jarPath -role node -hub http://localhost:4444/grid/register \
+-maxSession 25 -browser browserName=phantomjs,maxInstances=25
 
