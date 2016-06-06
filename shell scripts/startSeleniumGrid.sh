1918ee9b40f70703d025649e49d70808d87f9745
Paul Cruz <pcruz96@yahoo.com>
2016-06-05 21:15:09 -0700

    upd chromedriver path in startSeleniumGrid.sh

--- shell scripts/startSeleniumGrid.sh
+++ shell scripts/startSeleniumGrid.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-jarPath="../src/test/resources/selenium/selenium-server-standalone-2.53.0.jar"
+jarPath="src/test/resources/selenium/selenium-server-standalone-2.53.0.jar"
 
 java -jar $jarPath -role hub | \
-java -jar -Dwebdriver.chrome.driver="../src/test/resources/selenium/chromedriver.exe" $jarPath -role node -hub http://localhost:4444/grid/register \
--maxSession 25 -browser browserName=phantomjs,maxInstances=25
+java -jar -Dwebdriver.chrome.driver="src/test/resources/selenium/chromedriver" $jarPath -role node -hub http://localhost:4444/grid/register \
+#-maxSession 25 -browser browserName=phantomjs,maxInstances=25
 
