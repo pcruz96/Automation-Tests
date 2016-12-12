580091fcd0910af76a906b68488e1b542a8f20ae
Paul Cruz <pcruz@gomercatus.com>
2016-12-12 09:39:24 -0800

    added tableInOrder

--- src/test/java/com/automation/pageObjs/BasePage.java
+++ src/test/java/com/automation/pageObjs/BasePage.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import com.automation.selenium.Driver;
 import com.automation.selenium.SeleniumUtils;
@@ -26,10 +28,10 @@ public abstract class BasePage extends SeleniumUtils {
     	}
     	
     	this.threadSleep(5000);
-		List<String> aryList=new ArrayList();
-		List<String> aryList2=new ArrayList();
+		List<String> aryList=new ArrayList<String>();
+		List<String> aryList2=new ArrayList<String>();
 
-		for (int i = 1; i < 4; i++) {
+		for (int i = 1; i < 3; i++) {
 			String value = this.getTxt(By.cssSelector("tbody > tr:nth-child("+i+") > td:nth-child(" + child + ")"));
 			aryList.add(value);
 			aryList2.add(value);
@@ -41,6 +43,34 @@ public abstract class BasePage extends SeleniumUtils {
 			Collections.sort(aryList, Collections.reverseOrder());
 		}
 		
+		logger.info(aryList.toString());
+		logger.info(aryList2.toString());
+		
+		if (aryList.toString().equals(aryList2.toString())) {
+			return true;
+	    }
+		return false;
+	}
+    
+    public boolean tableInOrder(By obj) {
+		this.waitForElementVisibility(obj);
+		List<WebElement> elms = dvr.findElements(obj);
+		int i = 0;
+		List<String> aryList=new ArrayList<String>();
+		List<String> aryList2=new ArrayList<String>();
+		
+		for (WebElement we : elms) {
+			String txt = we.getText();
+			if (StringUtils.isAlphanumeric(txt.substring(0, 1))) {
+				aryList.add(txt);
+				aryList2.add(txt);
+				i++;
+				if (i == 3) {
+					break;
+				}
+			}
+		}
+		Collections.sort(aryList);	
 		logger.info(aryList.toString());
 		logger.info(aryList2.toString());
 		
