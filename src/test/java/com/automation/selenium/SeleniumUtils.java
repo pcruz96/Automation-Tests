a9b794883ee1cc478e2f398880414902313adef5
Paul Cruz <pcruz@gomercatus.com>
2017-01-09 13:12:37 -0800

    upd scrollIntoView

--- src/test/java/com/automation/selenium/SeleniumUtils.java
+++ src/test/java/com/automation/selenium/SeleniumUtils.java
@@ -679,6 +679,7 @@ public class SeleniumUtils extends Log4J {
 	}
 	
 	public void scrollIntoView(By obj) {
+		this.waitForPageLoaded();
 		WebElement element = driver.findElement(obj);
 		JavascriptExecutor executor = (JavascriptExecutor)driver;
 		executor.executeScript("arguments[0].scrollIntoView(true);", element);
