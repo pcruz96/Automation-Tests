ecb963a5ec0553c07c4904c59b86c258fb97dd16
Paul Cruz <pcruz@gomercatus.com>
2018-05-07 19:21:59 -0700

    fix sed for remainingTestRunCaseIds.txt

--- src/test/java/com/automation/tests/BaseTest.java
+++ src/test/java/com/automation/tests/BaseTest.java
@@ -439,7 +439,7 @@ public class BaseTest extends TestRailUtilities {
 		 //it's easier to retrieve the remaining list of caseIds to rerun 
 		ExecuteShellCommand es = new ExecuteShellCommand();
 		String runTxt = "remainingTestRunCaseIds.txt";
-		String[] cmd1 = new String[] {"sed", "-i.tmp", "s/"+BaseTest.getTestCaseId().replace("c", "")+"//g", runTxt};
+		String[] cmd1 = new String[] {"sed", "-i.tmp", "s/"+BaseTest.getTestCaseId().replace("c", "")+",//g", runTxt};
 		String[] cmd2 = new String[] {"sed", "-i.tmp", "s/,,//g", runTxt};
 		es.executeArrayCommand(cmd1);
 		es.executeArrayCommand(cmd2);
